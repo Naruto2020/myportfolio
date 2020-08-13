1c965c2ddf21490ce3b2e152cf40e8391699f8f6
Naruto2020 <guyfredy@gmail.com>
2020-08-13 13:52:18 +0200

    mon portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,110 @@
+var $Bouton = $(".wrapper button");
+var $LienP = $(".btnScroll");
+var $ProfJs = $(".profJs");
+var $LienP1 = $("#Profil");
+var $ExpJs = $(".expJs");
+var $LienP2 = $("#Experiences");
+var $CapJs = $(".capJs");
+var $LienP3 = $("#Capacites");
+var $ProjJs = $(".projJs");
+var $LienP4 = $("#Projets");
+var $ContJs = $(".contJs");
+var $LienP5 = $("#Contact");
+var $Section = $("section");
+var $Menu = $(".navbar-nav");
+var speed = 650; // Durée de l'animation (en ms)
+
+// deplacement animé des ancres
+var ancres = function () {
+  $(".wrapper button").click(function () {
+    $("html, body").animate({ scrollTop: $(".btnScroll").offset().top }, speed); // Go
+    return false;
+  });
+
+  $(".profJs").click(function () {
+    $("html, body").animate({ scrollTop: $("#Profil").offset().top }, speed); // Go
+    return false;
+  });
+
+  $(".expJs").click(function () {
+    $("html, body").animate(
+      { scrollTop: $("#Experiences").offset().top },
+      speed
+    );
+    return false;
+  });
+
+  $(".capJs").click(function () {
+    $("html, body").animate({ scrollTop: $("#Capacites").offset().top }, speed);
+    return false;
+  });
+
+  $(".projJs").click(function () {
+    $("html, body").animate({ scrollTop: $("#Projets").offset().top }, speed);
+    return false;
+  });
+
+  $(".contJs").click(function () {
+    $("html, body").animate({ scrollTop: $("#Contact").offset().top }, speed);
+    return false;
+  });
+};
+
+// animation background menu en fonction du scroll
+var scro = function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 700) {
+      $(".menu").addClass("scrolling");
+    } else {
+      $(".menu").removeClass("scrolling");
+    }
+  });
+};
+
+// animation du background des projets
+var ecrans = function () {
+  var $EcranP = $(".proAJs figure figcaption");
+  var $EcranP1 = $(".proBJs figure figcaption");
+  var $EcranP2 = $(".proAJs1 figure figcaption");
+  var $EcranP3 = $(".proBJs1 figure figcaption");
+
+  $(".proAJs img").mouseover(function () {
+    $EcranP.css("display", "block");
+  });
+
+  $(".proAJs img").mouseout(function () {
+    $EcranP.css("display", "none");
+  });
+
+  $(".proBJs img").mouseover(function () {
+    $EcranP1.css("display", "block");
+  });
+
+  $(".proBJs img").mouseout(function () {
+    $EcranP1.css("display", "none");
+  });
+
+  $(".proAJs1 img").mouseover(function () {
+    $EcranP2.css("display", "block");
+  });
+
+  $(".proAJs1 img").mouseout(function () {
+    $EcranP2.css("display", "none");
+  });
+
+  $(".proBJs1 img").mouseover(function () {
+    $EcranP3.css("display", "block");
+  });
+
+  $(".proBJs1 img").mouseout(function () {
+    $EcranP3.css("display", "none");
+  });
+};
+
+$(function () {
+  ancres();
+  scro();
+  ecrans();
+});
